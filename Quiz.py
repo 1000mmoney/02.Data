16e4a99a3c4577df0337c5cd2f4dfa6fa8cbe733
1000mmoney <gustn8425@naver.com>
2024-07-02 17:22:55 +0900

    퀴즈
    회원가입
    환율계산

---
+++ Quiz.py
@@ -0,0 +1,19 @@
+name = input("이름을 입력하시오 :")
+age = input("나이를 입력하시오 :" )
+email = input("이메일을 입력하시오 :" )
+tell = input("연락처를 입력하시오 :" )
+
+dic= {name : [age, email, tell]}
+print(dic)
+
+name_1 = input("이름을 입력하시오 :")
+age_1 = input("나이를 입력하시오 :" )
+email_1 = input("이메일을 입력하시오 :" )
+tell_1 = input("연락처를 입력하시오 :" )
+
+dic_1= {name_1: [age, email, tell]}
+print(dic_1)
+
+
+
+
