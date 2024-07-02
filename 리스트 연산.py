917d760374a0aec8a8fea215cc6b4676fd470c6b
1000mmoney <gustn8425@naver.com>
2024-07-02 16:15:40 +0900

    변수
    변수 데이터 타입 및 리스트
    리스트 연산 및 리스트 추출 인덱싱, 슬라이싱
    불린 참값 거짓값 찾기

---
+++ 리스트 연산.py
@@ -0,0 +1,13 @@
+number = [1,2,3,4,5]
+result = number[2] + number[-1]
+print(result)
+
+print(len(number))
+
+# 리스트에 요소 추가하기, 삭제하기
+last = [1,2,3]
+last.append(30)
+print(last)
+last.remove(3)
+print(last)
+print(type(last))
---
+++ 불린.py
@@ -0,0 +1,5 @@
+is_true = True
+is_false = False
+
+print(1==0.9)
+print(type(is_true))
